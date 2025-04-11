4194f351210a7e37fcd0b9964bf10451b2a702af
Emir Selengil <emirselengil26@gmail.com>
2025-04-11 19:34:26 +0300

    first-commit

---
+++ src/main/java/com/ilerijava_entegrasyon/BirfaturaApiEntegrasyonApplication.java
@@ -0,0 +1,13 @@
+package com.ilerijava_entegrasyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirfaturaApiEntegrasyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirfaturaApiEntegrasyonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilerijava_entegrasyon/BirfaturaApiEntegrasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilerijava_entegrasyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirfaturaApiEntegrasyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
